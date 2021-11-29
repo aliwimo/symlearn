d9ca1a5c658008ec82ad17f58a3a7a3d80fb8986
aliwimo <aliwimo@gmail.com>
2021-11-29 22:28:11 +0300

    modified protected division function

--- expressions.py
+++ expressions.py
@@ -11,6 +11,9 @@ class Add(Node):
     def __repr__(self):
         return '+'
 
+    def get_id(self):
+        return self.id
+
     def output(self, X):
         X0 = self.left.output(X)
         X1 = self.right.output(X)
@@ -24,6 +27,9 @@ class Sub(Node):
 
     def __repr__(self):
         return '-'
+
+    def get_id(self):
+        return self.id
 
     def output(self, X):
         X0 = self.left.output(X)
@@ -39,6 +45,9 @@ class Mul(Node):
     def __repr__(self):
         return '*'
 
+    def get_id(self):
+        return self.id
+
     def output(self, X):
         X0 = self.left.output(X)
         X1 = self.right.output(X)
@@ -52,14 +61,20 @@ class Div(Node):
         self.type = 'function'
 
     def __repr__(self):
-        return '*'
+        return '/'
+
+    def get_id(self):
+        return self.id
 
     def output(self, X):
         X0 = self.left.output(X)
         X1 = self.right.output(X)
-        sign_X1 = np.sign(X1)
+        sign_X1 = np.sign(X1)   # numpy.sign: X>=1 returns 1, X==0 returns 0, X<=1 returns -1 
         sign_X1[sign_X1 == 0] = 1
-        return np.multiply(sign_X1, X0) / ( 1e-6 + np.abs(X1))
+        return X0 / sign_X1
+        # sign_X1 = np.sign(X1)
+        # sign_X1[sign_X1 == 0] = 1
+        # return np.multiply(sign_X1, X0) / ( 1e-6 + np.abs(X1))
 
 
 class Sin(Node):
@@ -70,6 +85,9 @@ class Sin(Node):
 
     def __repr__(self):
         return 'sin'
+
+    def get_id(self):
+        return self.id
 
     def output(self, X):
         X0 = self.right.output(X)
@@ -85,11 +103,61 @@ class Cos(Node):
     def __repr__(self):
         return 'cos'
 
+    def get_id(self):
+        return self.id
+
     def output(self, X):
         X0 = self.right.output(X)
         return np.cos(X0)
 
 
+class Pow(Node):
+    def __init__(self):
+        super(Pow, self).__init__()
+        self.inputs = 2
+
+    def __repr__(self):
+        return 'pow'
+
+    def get_id(self):
+        return self.id
+
+    def output(self, X):
+        X0 = self.left.output(X)
+        X1 = self.right.output(X)
+        return np.power(X0, X1)
+
+
+class Exp(Node):
+    def __init__(self):
+        super(Exp, self).__init__()
+        self.inputs = 1
+
+    def __repr__(self):
+        return 'exp'
+
+    def get_id(self):
+        return self.id
+
+    def output(self, X):
+        X0 = self.right.output(X)
+        return np.exp(X0)
+
+
+class Rlog(Node):
+    def __init__(self):
+        super(Rlog, self).__init__()
+        self.inputs = 1
+
+    def __repr__(self):
+        return 'rlog'
+
+    def get_id(self):
+        return self.id
+
+    def output(self, X):
+        X0 = self.right.output(X)
+        return np.log(np.abs(X0) + 1e-6)
 
 class Variable(Node):
     def __init__(self):
@@ -99,6 +167,9 @@ class Variable(Node):
 
     def __repr__(self):
         return 'x' + str(self.rank)
+
+    def get_id(self):
+        return self.id
 
     def output(self, X):
         return X[:, self.rank]
@@ -112,6 +183,9 @@ class Constant(Node):
 
     def __repr__(self):
         return str(self.value)
+
+    def get_id(self):
+        return self.id
 
     def output(self, X):
         return np.array([self.value] * X.shape[0])
