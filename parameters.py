da2ddb285e862c0283d425d1827e4f4adb8100a1
aliwimo <aliwimo@gmail.com>
2021-12-16 00:15:10 +0300

    reformed method shared parameters

--- parameters.py
+++ parameters.py
@@ -1,16 +1,6 @@
 class Parameters:
-    POP_SIZE = 0
-    MAX_EVAL = 0
-    MAX_GEN = 0
-    INIT_MIN_DEPTH = 0
-    INIT_MAX_DEPTH = 0
-    MAX_DEPTH = 0
-    DOMAIN_X = []
-    DOMAIN_Y = []
-    VAR_NUM = 0
-    POINT_NUM = 0
-    OPERATORS = ['+', '-', '*', '/']
-    FUNCTIONS = ['sin', 'cos', 'exp', 'rlog']
-    VARIABLES = ['x0']
-    CONSTANTS = range(1, 2)
+    OPERATORS = []
+    FUNCTIONS = []
+    VARIABLES = []
+    CONSTANTS = []
 
