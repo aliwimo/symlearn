4b8e1c0c5595157ad21a014b5efd3b631cb50cfa
Mohamed Aliwi <aliwimo@gmail.com>
2022-12-28 19:33:41 +0300

    Add model class inheritance

--- models/ipp.py
+++ models/ipp.py
@@ -1,37 +1,39 @@
 import numpy as np
 from copy import deepcopy
 from random import random
-from sklearn.metrics import r2_score
 from datetime import datetime, timedelta
 from core.methods import Methods
 from core.functions import *
+from models.model import Model
 
-class IPP:
+
+class IPP(Model):
 
     def __init__(self,
-                pop_size=100,
-                donors_number=1,
-                receivers_number=1,
-                max_evaluations=10000,
-                max_generations=-1,
-                max_time=None,
-                initial_min_depth=0,
-                initial_max_depth=6,
-                min_depth=1,
-                max_depth=15,
-                error_function=None,
-                expressions=[Add, Sub, Mul],
-                terminals=[Variable, Constant],
-                target_error=0.0, 
-                verbose=True
-                ):
+                 pop_size=100,
+                 donors_number=1,
+                 receivers_number=1,
+                 max_evaluations=10000,
+                 max_generations=-1,
+                 max_time=None,
+                 initial_min_depth=0,
+                 initial_max_depth=6,
+                 min_depth=1,
+                 max_depth=15,
+                 error_function=None,
+                 expressions=[Add, Sub, Mul],
+                 terminals=[Variable, Constant],
+                 target_error=0.0,
+                 verbose=True
+                 ):
 
+        super(IPP, self).__init__(max_evaluations,
+                                  max_generations,
+                                  max_time,
+                                  verbose)
         self.pop_size = pop_size
         self.donors_number = donors_number
         self.receivers_number = receivers_number
-        self.max_evaluations = max_evaluations
-        self.max_generations = max_generations
-        self.max_time = max_time
         self.initial_min_depth = initial_min_depth
         self.initial_max_depth = initial_max_depth
         self.min_depth = min_depth
@@ -41,72 +43,50 @@ class IPP:
         self.terminals = terminals
         self.target_error = target_error
         self.verbose = verbose
-        self.current_evaluation = 0
-        self.current_generation = 0
-        self.best_individual = None
         self.population = None
         self.fitnesses = None
 
     def fit(self, X, y):
         self.X = X
         self.y = y
-        if self.max_time: 
+        if self.max_time:
             self.start_time = datetime.now()
             self.end_time = self.start_time + timedelta(seconds=self.max_time)
         self.generate_population()
         self.get_initial_statistics()
         self.run()
-        if self.verbose: 
-            if self.max_time: print(f'Total time: {datetime.now() - self.start_time}')
+        if self.verbose:
+            if self.max_time:
+                print(f'Total time: {datetime.now() - self.start_time}')
             print(f'Evaluations: {self.current_evaluation}')
-
-    def score(self, y_test, y_pred):
-        return r2_score(y_test, y_pred)
-
-    def predict(self, X):
-        return self.best_individual.output(X)
 
     def generate_population(self):
         self.population = Methods.generate_population(
-                            pop_size=self.pop_size,
-                            initial_min_depth=self.initial_min_depth,
-                            initial_max_depth=self.initial_max_depth,
-                            expressions=self.expressions,
-                            terminals=self.terminals)
+            pop_size=self.pop_size,
+            initial_min_depth=self.initial_min_depth,
+            initial_max_depth=self.initial_max_depth,
+            expressions=self.expressions,
+            terminals=self.terminals)
 
     def get_initial_statistics(self):
         self.fitnesses = [0] * self.pop_size
         min_error = 10e6
         min_index = -1
         for index in range(self.pop_size):
-            self.population[index].update_fitness(self.error_function, self.X, self.y)
+            self.population[index].update_fitness(
+                self.error_function, self.X, self.y)
             self.fitnesses[index] = self.population[index].fitness
-            if self.population[index].fitness <= min_error: min_index = index
-        self.best_individual = deepcopy(self.population[min_index])
-        self.best_individual.update_fitness(self.error_function, self.X, self.y)
-        self.best_individual.update_fitness(self.error_function, self.X, self.y)
-
-    def must_terminate(self):
-        terminate = False
-        if self.max_time and datetime.now() > self.end_time: 
-            terminate = True
-        elif self.max_evaluations > -1 and self.current_evaluation > self.max_evaluations:
-            terminate = True
-        elif self.max_generations >-1 and self.current_generation > self.max_generations:
-            terminate = True
-        elif self.best_individual.fitness < self.target_error:
-            terminate = True
-        return terminate
+            if self.population[index].fitness <= min_error:
+                min_index = index
+        self.model = deepcopy(self.population[min_index])
+        self.model.update_fitness(
+            self.error_function, self.X, self.y)
+        self.model.update_fitness(
+            self.error_function, self.X, self.y)
 
     def rank(self, is_reversed=False):
-        self.population, self.fitnesses = Methods.rank_trees(self.population, self.fitnesses, is_reversed)
-    
-    def export_best(self, export_path='images/', filename='Best'):
-        if self.best_individual:
-            label = "Best error: "
-            label += str(round(self.best_individual.fitness, 3))
-            Methods.export_graph(self.best_individual, export_path + filename, label)
-            print(self.best_individual.equation())
+        self.population, self.fitnesses = Methods.rank_trees(
+            self.population, self.fitnesses, is_reversed)
 
     def attract(self, i, j):
         return Methods.share(self.population[j], deepcopy(self.population[i]))
@@ -116,15 +96,11 @@ class IPP:
         if temp.fitness < self.population[current].fitness:
             self.population[current] = deepcopy(temp)
             self.fitnesses[current] = self.population[current].fitness
-            if self.population[current].fitness < self.best_individual.fitness:
-                self.best_individual = deepcopy(self.population[current])
+            if self.population[current].fitness < self.model.fitness:
+                self.model = deepcopy(self.population[current])
                 if self.verbose:
-                    print(f'Evaluations: {self.current_evaluation} | Fitness: {self.best_individual.fitness}')
-    
-    def test_model(self, X):
-        zero_array = np.zeros(X.shape)
-        x = np.vstack([zero_array, X])
-        return self.best_individual.output(x)[-1]
+                    print(
+                        f'Evaluations: {self.current_evaluation} | Fitness: {self.model.fitness}')
 
     # perform infection between two individuals
     def perform_infection(self, individual):
@@ -145,9 +121,7 @@ class IPP:
             receivers.append(sorted_indexes[-1 - i])
         return donors, receivers
 
-    # standard firefly programming method (FP)
     def run(self):
-
         while not self.must_terminate():
             self.rank(is_reversed=False)
 
@@ -158,14 +132,18 @@ class IPP:
                 while random_index == index:
                     random_index = np.random.randint(0, self.pop_size)
                 current_individual = deepcopy(self.population[index])
-                infected_individual = self.perform_infection(current_individual)
+                infected_individual = self.perform_infection(
+                    current_individual)
                 if infected_individual.depth() > self.max_depth or infected_individual.depth() < self.min_depth:
                     if random() > 0.5:
-                        infected_individual = Methods.generate_individual('full', self.initial_min_depth, self.initial_max_depth, self.expressions, self.terminals)
+                        infected_individual = Methods.generate_individual(
+                            'full', self.initial_min_depth, self.initial_max_depth, self.expressions, self.terminals)
                     else:
-                        infected_individual = Methods.generate_individual('grow', self.initial_min_depth, self.initial_max_depth, self.expressions, self.terminals)
+                        infected_individual = Methods.generate_individual(
+                            'grow', self.initial_min_depth, self.initial_max_depth, self.expressions, self.terminals)
                 self.evalualte(index, infected_individual)
-                if self.must_terminate(): break
+                if self.must_terminate():
+                    break
 
             # start of plasma transfering phase
             # generating dose_control and treatment_control vectors
@@ -174,55 +152,70 @@ class IPP:
 
             # get indexes of both of donors and receivers
             donors_indexes, receivers_indexes = self.get_donors_and_receivers_indexes()
-            
+
             for index in range(self.receivers_number):
                 receiver_index = receivers_indexes[index]
-                random_donor_index = donors_indexes[int(np.random.randint(0, self.donors_number))]
+                random_donor_index = donors_indexes[int(
+                    np.random.randint(0, self.donors_number))]
                 current_receiver = self.population[receiver_index]
                 random_donor = self.population[random_donor_index]
                 while treatment_control[index] == 1:
-                    if self.must_terminate(): break
+                    if self.must_terminate():
+                        break
                     self.current_evaluation += 1
-                    treated_individual = self.perform_plasma_transfer(current_receiver, random_donor)
-                    treated_individual.update_fitness(self.error_function, self.X, self.y)      
+                    treated_individual = self.perform_plasma_transfer(
+                        current_receiver, random_donor)
+                    treated_individual.update_fitness(
+                        self.error_function, self.X, self.y)
                     if dose_control[index] == 1:
                         if treated_individual.fitness < self.fitnesses[random_donor_index]:
                             dose_control[index] += 1
-                            self.population[receiver_index] = deepcopy(treated_individual)
+                            self.population[receiver_index] = deepcopy(
+                                treated_individual)
                             self.fitnesses[receiver_index] = treated_individual.fitness
                         else:
-                            self.population[receiver_index] = deepcopy(random_donor)
+                            self.population[receiver_index] = deepcopy(
+                                random_donor)
                             self.fitnesses[receiver_index] = self.fitnesses[random_donor_index]
                             treatment_control[index] = 0
                     else:
                         if treated_individual.fitness < self.fitnesses[receiver_index]:
-                            self.population[receiver_index] = deepcopy(treated_individual)
+                            self.population[receiver_index] = deepcopy(
+                                treated_individual)
                             self.fitnesses[receiver_index] = treated_individual.fitness
                         else:
                             treatment_control[index] = 0
-                        
-                    if self.population[receiver_index].fitness < self.best_individual.fitness:
-                        self.best_individual = deepcopy(self.population[receiver_index])
-                if self.must_terminate(): break
 
+                    if self.population[receiver_index].fitness < self.model.fitness:
+                        self.model = deepcopy(
+                            self.population[receiver_index])
+                if self.must_terminate():
+                    break
 
             # start of donors updating phase
             for index in range(self.donors_number):
-                if self.must_terminate(): break
+                if self.must_terminate():
+                    break
                 self.current_evaluation += 1
                 donor_index = donors_indexes[index]
                 if (self.current_evaluation / self.max_evaluations) > random():
-                    temp = self.perform_infection(self.population[donor_index]) # the same of update donor here!
+                    # the same of update donor here!
+                    temp = self.perform_infection(self.population[donor_index])
                 else:
                     if random() > 0.5:
-                        temp = Methods.generate_individual('full', self.initial_min_depth, self.initial_max_depth, self.expressions, self.terminals)
+                        temp = Methods.generate_individual(
+                            'full', self.initial_min_depth, self.initial_max_depth, self.expressions, self.terminals)
                     else:
-                        temp = Methods.generate_individual('grow', self.initial_min_depth, self.initial_max_depth, self.expressions, self.terminals)
-                temp.update_fitness(self.error_function, self.X, self.y) 
+                        temp = Methods.generate_individual(
+                            'grow', self.initial_min_depth, self.initial_max_depth, self.expressions, self.terminals)
+                temp.update_fitness(self.error_function, self.X, self.y)
                 self.population[donor_index] = deepcopy(temp)
                 self.fitnesses[donor_index] = self.population[donor_index].fitness
-                if self.population[donor_index].fitness < self.best_individual.fitness:
-                    self.best_individual = deepcopy(self.population[donor_index])
-                if self.must_terminate(): break
+                if self.population[donor_index].fitness < self.model.fitness:
+                    self.model = deepcopy(
+                        self.population[donor_index])
+                if self.must_terminate():
+                    break
 
-            if not self.max_generations == -1: self.current_generation += 1
+            if not self.max_generations == -1:
+                self.current_generation += 1
