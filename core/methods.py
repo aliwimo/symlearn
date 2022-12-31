c23945994a04b9695f6849d24d3aefa1d0032ee4
Mohamed Aliwi <aliwimo@gmail.com>
2022-12-31 20:40:05 +0300

    Add methods documentation

--- core/methods.py
+++ core/methods.py
@@ -1,17 +1,49 @@
+"""Methods used in evaluation process.
+
+The :mod:`methods` module contains a set of different methods that is 
+used for modifying programmes during optimization process.
+"""
+
+from random import random, choice
+from copy import deepcopy
 import numpy as np
 import matplotlib.pyplot as plt
-from random import random, choice
 from graphviz import Digraph, Source  # install version 0.16
-from copy import deepcopy
 from core.parameters import Parameters
 from core.node import Node
 from core.functions import Constant
 
 
 class Methods:
+    """Methods used in evaluation process.
+
+    This class contains a set of different methods that is 
+    used for modifying programmes during optimization process.
+    """
 
     @classmethod
-    def generate_population(cls, pop_size, initial_min_depth, initial_max_depth, expressions, terminals):
+    def generate_population(cls,
+                            pop_size,
+                            initial_min_depth,
+                            initial_max_depth,
+                            expressions,
+                            terminals):
+        """Generate the initial population for population-based models.
+
+        This method generates population using `ramped-half-and-half` 
+        method which is a combination of `full` and `grow` forming
+        methods. 
+
+        Args:
+            pop_size (int): The size of the generated population
+            initial_min_depth (int): Minimum trees forming height
+            initial_max_depth (int): Maximum trees forming height
+            expressions (list): Set of different expression used in tree
+            terminals (list): Set of different terminals used in tree
+
+        Returns:
+            Array of trees that represent the population
+        """
         population = []
         # generate with full method
         for _ in range(pop_size // 2):
@@ -33,6 +65,22 @@ class Methods:
                             expressions,
                             terminals,
                             current_depth=0) -> Node:
+        """Generate an individual tree (node).
+
+        This method generates an individual using either 
+        `full` or`grow` forming method.
+
+        Args:
+            method (string): The size of the generated population
+            initial_min_depth (int): Minimum trees forming height
+            initial_max_depth (int): Maximum trees forming height
+            expressions (list): Set of different expression used in tree
+            terminals (list): Set of different terminals used in tree
+            current_depth (int): Tree's depth controlling argument
+
+        Returns:
+            Individual tree's head (node).
+        """
         if method == 'full':
             if current_depth < initial_max_depth - 1:
                 node = choice(expressions)()
@@ -64,6 +112,18 @@ class Methods:
 
     @classmethod
     def share(cls, source: Node, target: Node):
+        """Performs sharing operation between two trees.
+
+        This method takes an instance subtree from source and glues
+        it to the target.
+
+        Args:
+            source (node): The source tree head (node)
+            target (node): The target tree head (node)
+
+        Returns:
+            The new generated tree's head (node).
+        """
         source_nodes = source.sub_nodes()
 
         if len(source_nodes) > 1:
@@ -93,6 +153,18 @@ class Methods:
 
     @classmethod
     def change_node(cls, source: Node, nodes_pool):
+        """Performs replacement operation in one tree.
+
+        This method takes selects a random node in the source
+        tree and replaces it with a same-arity node.
+
+        Args:
+            source (node): The source tree head (node)
+            nodes_pool (list): List of possible replacement nodes
+
+        Returns:
+            The new generated tree's head (node).
+        """
         source_nodes = source.sub_nodes()
         selected_node = choice(source_nodes)
         same_inputs = False
@@ -118,6 +190,19 @@ class Methods:
 
     @classmethod
     def rank_trees(cls, trees, fitnesses, is_reversed=False):
+        """Ranks trees according to fitness rank.
+
+        This method ranks trees after sorting the fitness values.
+
+        Args:
+            trees (list): List of trees' heads (nodes)
+            fitnesses (list): List of fitness values of trees
+            is_reversed (bool): Reversed sorting modifier
+
+        Returns:
+            Trees: List of sorted trees' heads (nodes)
+            fitnesses: List of sorted fitness values
+        """
         sorted_indices = np.argsort(fitnesses)
         if not is_reversed:
             sorted_indices = np.flip(sorted_indices)
@@ -129,6 +214,15 @@ class Methods:
 
     @classmethod
     def export_graph(cls, root: Node, file_name, label):
+        """Exports the graph of a tree.
+
+        This method exports the graph of a tree.
+
+        Args:
+            root (node): Exported tree's head (node)
+            filename (string): Exported file name
+            label (string): Label displayed in the exported graph
+        """
         graph = [Digraph()]
         graph[0].attr(kw='graph', label=label)
         cls.draw_node(graph, root)
@@ -137,6 +231,14 @@ class Methods:
 
     @classmethod
     def draw_node(cls, graph, root: Node):
+        """This is an `export_graph` sub function.
+
+        This method draws single tree's node.
+
+        Args:
+            graph (node): The graph that contains the node
+            root (node): Exported tree's head (node)
+        """
         graph[0].node(str(root.id), str(root))
         if root.left:
             graph[0].edge(str(root.id), str(root.left.id))
@@ -147,6 +249,18 @@ class Methods:
 
     @classmethod
     def simplify(cls, root: Node):
+        """Simplifies tree by combining nodes.
+
+        This method simplifies trees by combining tree's branches
+        that does not haveany variable nodes.
+
+        Args:
+            root (node): Simplified tree's head (node)
+
+        Returns:
+            A boolean modifier that checks if current node has a
+            variable child.
+        """
         has_variables = False
         if root.type == 'variable':
             has_variables = True
@@ -170,8 +284,26 @@ class Methods:
         return has_variables
 
     @classmethod
-    def plot(cls, x_axis_train, y_axis_train, y_axis_fitted, x_axis_test=None, y_axis_test=None, y_axis_pred=None, test_set=False):
+    def plot(cls, x_axis_train,
+             y_axis_train,
+             y_axis_fitted,
+             x_axis_test=None,
+             y_axis_test=None,
+             y_axis_pred=None,
+             test_set=False):
+        """Plots training and testing predicted graphs.
 
+        This method plots training and testing predicted graphs.
+
+        Args:
+            x_axis_train (list): Training set `x` original data values
+            y_axis_train (list): Training set `y` original data values
+            y_axis_fitted (list): Training set `y` predicted values
+            x_axis_test (list): Testing set `x` original data values
+            y_axis_test (list): Testing set `y` original data values
+            y_axis_pred (list): Testing set `y` predicted values
+            test_set (bool): Controlling modifier of testing set
+        """
         # adding additional point to remove the spae between train and test sets
         if test_set:
             x_axis_train = np.append(x_axis_train, x_axis_test[0])
