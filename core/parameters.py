da9eb3571288c5eab1c94dc1190f71b1caa5bc18
Mohamed Aliwi <aliwimo@gmail.com>
2023-01-03 21:51:53 +0300

    Add parameters class documentation

--- core/parameters.py
+++ core/parameters.py
@@ -1,4 +1,18 @@
+"""Global parameters.
+
+The :mod:`parameters` module contains a set of different global parameters that is 
+used whıle solvıng problems.
+"""
+
 class Parameters:
+    """Parameters class.
+
+    Attributes:
+        CONSTANTS (list): List of constants used in nodes
+        FEATURES (int): Number of unique variables
+        CONSTANTS_TYPE (string): Constant type modifier
+        EXPORT_EXT (string): Export graph extension
+    """
     CONSTANTS = [0, 1]
     FEATURES = 1
     CONSTANTS_TYPE = 'range'
