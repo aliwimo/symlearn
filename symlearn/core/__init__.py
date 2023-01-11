a58af7dd12b772cf408f791110f3fd3e9327235e
Mohamed Aliwi <aliwimo@gmail.com>
2023-01-12 02:32:11 +0300

    Update `__init__` file

---
+++ symlearn/core/__init__.py
@@ -0,0 +1,11 @@
+# errors
+from symlearn.core.errors import mean_absolute_error_c
+from symlearn.core.errors import mean_squared_error_c
+from symlearn.core.errors import r2_score
+from symlearn.core.errors import r2_score_inverse
+from symlearn.core.errors import root_mean_squared_error
+from symlearn.core.errors import sum_of_difference
+
+
+from symlearn.models.dffp import DFFP
+from symlearn.models.ipp import IPP
