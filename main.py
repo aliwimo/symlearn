42be418baadd320c6a016e7fb0c903e49c2da898
Mohamed Aliwi <aliwimo@gmail.com>
2023-01-20 17:14:40 +0300

    Update main file

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from symlearn.core.functions import *
 from symlearn.core.errors import *
 
 # import models
-from symlearn.models import FFP, DFFP, IPP
+from symlearn.models import FFP, DFFP, IPP, GP
 
 # suppress numpy warnings
 np.seterr(all='ignore')
@@ -93,6 +93,31 @@ model = IPP(pop_size=100,
             target_error=0,
             verbose=False
             )
+
+# fit data into model
+model.fit(X_train, y_train)
+y_fit = model.predict(X_train)
+y_pred = model.predict(X_test)
+
+# print results of the model
+train_score = sum_of_difference(y_train, y_fit)
+test_score = sum_of_difference(y_test, y_pred)
+print(f'Training set r2 score: {train_score}\nTest set r2 score: {test_score}')
+
+
+print("GP")
+model = GP(pop_size=50,
+        max_evaluations=2500,
+        initial_min_depth=0,
+        initial_max_depth=6,
+        min_depth=1,
+        max_depth=15,
+        error_function=sum_of_difference,
+        expressions=expressions,
+        terminals=terminals,
+        target_error=0,
+        verbose=False
+        )
 
 # fit data into model
 model.fit(X_train, y_train)
