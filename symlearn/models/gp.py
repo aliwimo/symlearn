64284842b7817f487a87a7d72d861ad6af724286
Mohamed Aliwi <aliwimo@gmail.com>
2023-01-30 04:25:49 +0300

    Update `GP` model

--- symlearn/models/gp.py
+++ symlearn/models/gp.py
@@ -1,9 +1,9 @@
 import numpy as np
-from copy import deepcopy
-from random import random, randint
+from copy import deepcopy, copy
+from random import random, randint, choices
 from datetime import datetime, timedelta
 from symlearn.core.methods import Methods
-from symlearn.core.operators import share
+from symlearn.core.operators import share, substitute
 from symlearn.core.functions import *
 from symlearn.models.model import Model
 
@@ -27,8 +27,11 @@ class GP(Model):
                  expressions=[Add, Sub, Mul],
                  terminals=[Variable, Constant],
                  target_error=0.0,
-                 n_elite=2,
-                 p_crossover=0.9,
+                 elite_number=2,
+                 crossover_probability=0.9,
+                 mutation_probability=0.1,
+                 tournament_size=4,
+                 selection_method='tournament', # possible choices: wheel, tournament
                  verbose=True
                  ):
         """Initializes the GP algorithm."""
@@ -46,8 +49,16 @@ class GP(Model):
             terminals=terminals,
             target_error=target_error,
             verbose=verbose)
-        self.n_elite = n_elite
-        self.p_crossover = p_crossover
+
+        self.elite_number = elite_number
+        self.crossover_probability = crossover_probability
+        self.mutation_probability = mutation_probability
+        self.tournament_size = tournament_size
+        if selection_method == 'tournament':
+            self.selection_method = self.tournament_selection
+        elif selection_method == 'wheel':
+            self.selection_method = self.wheel_selection
+        
 
     def fit(self, X, y):
         """
@@ -74,16 +85,33 @@ class GP(Model):
             print(f'Evaluations: {self.current_evaluation}')
 
     
-    # roulette wheel selection
     def perform_selection(self):
-        return self.rand_selection()
+        return self.selection_method()
 
-    # def wheel_selection(self):
-    #     total_fit = sum(self.fitnesses)
-    #     prop = [(i / total_fit) for i in fitnesses]
-    #     chosen = np.random.choice(chromosome_number, p=prop, size=2) # 
-    #     return population[chosen[0]], population[chosen[1]]
-    
+    def wheel_selection(self):
+        # since its a minimization problem, 
+        # its necessary to get the inverse values of fitnesses
+        fitnesses_inverse = 1 / np.array(self.fitnesses)
+        total_sum = sum(fitnesses_inverse)
+        probabilities = [(i / total_sum) for i in fitnesses_inverse]
+        chosen = np.random.choice(self.pop_size, p=probabilities, size=2)
+        return self.population[chosen[0]], self.population[chosen[1]]
+
+    def tournament_selection(self):
+        parents = [0] * self.tournament_size
+        parents_fitnesses = [0] * self.tournament_size
+        indexes = choices(range(self.pop_size), k=self.tournament_size)
+        for i in range(self.tournament_size):
+            parents[i] = deepcopy(self.population[indexes[i]])
+            parents_fitnesses[i] = self.fitnesses[indexes[i]].copy()
+        parents, parents_fitnesses = Methods.rank_trees(self.population, self.fitnesses)
+        return parents[0], parents[1]
+
+    def perform_mutation(self, tree):
+        tree = substitute(tree, self.expressions + self.terminals)
+        return self._check_depth(tree)
+
+
     def rand_selection(self):
         index1 = randint(0, self.pop_size - 1)
         index2 = randint(0, self.pop_size - 1)
@@ -92,10 +120,9 @@ class GP(Model):
     def perform_crossover(self, parent1, parent2):
         child1 = share(deepcopy(parent2), deepcopy(parent1))
         child2 = share(deepcopy(parent1), deepcopy(parent2))
-        if child1.depth() > self.max_depth or child1.depth() < self.min_depth:
-            child1 = self._generate_random_tree()
-        if child2.depth() > self.max_depth or child2.depth() < self.min_depth:
-            child2 = self._generate_random_tree()
+        # check depth after sharing
+        child1 = self._check_depth(child1)
+        child2 = self._check_depth(child2)
         return child1, child2
 
     def _run(self):
@@ -112,26 +139,31 @@ class GP(Model):
             self._rank(is_reversed=False)
 
             # copy elites to the new generation
-            for i in range(self.n_elite): 
+            for i in range(self.elite_number): 
                 next_generation[i] = deepcopy(self.population[i])
 
             # perform crossover
-            for i in range(self.n_elite, self.pop_size, 2):
+            for i in range(self.elite_number, self.pop_size, 2):
                 parent1, parent2 = self.perform_selection()
-                if random() < self.p_crossover:
+                if random() < self.crossover_probability:
                     child1, child2 = self.perform_crossover(parent1, parent2)
                     next_generation[i] = deepcopy(child1)
                     next_generation[i + 1] = deepcopy(child2)
                 else:
                     next_generation[i] = deepcopy(parent1)
                     next_generation[i + 1] = deepcopy(parent2)
-            
+
+            # perform mutation
+            for i in range(self.pop_size):
+                if random() < self.mutation_probability:
+                    next_generation[i] = self.perform_mutation(deepcopy(self.population[i]))
+
             # update and calculate the fitnesses of the next generation and find the best
             self._update_errors(next_generation)
             next_fitnesses = self._calculate_errors(next_generation)
             next_generation_best = self._find_best_model(next_generation, next_fitnesses)
 
-
+            # comparing current and next generations
             for i in range(self.pop_size):
                 if next_fitnesses[i] > self.fitnesses[i]:
                     self.population[i] = deepcopy(next_generation[i])
@@ -139,7 +171,8 @@ class GP(Model):
 
             # increase evaluation counter
             self.current_evaluation += self.pop_size
-
+            
+            # compare with best model
             self._compare_model(next_generation_best)
             
             # increase generation counter
